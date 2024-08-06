d379cb55eaafc6faeaf93f1f4bf251ee36e43536
Kenjiro Morimoto <morimoto@katfuji.lab.tuat.ac.jp>
2024-08-06 13:42:12 +0900

    remove dealornodeal and mutualfriends

--- dealornodeal/sessions/session.py
+++
@@ -1,9 +0,0 @@
-from cocoa.sessions.session import Session as BaseSession
-from core.event import Event
-
-class Session(BaseSession):
-    def select(self, outcome, metadata=None):
-        return Event.SelectEvent(self.agent, data=outcome, time=self.timestamp(), metadata=metadata)
-
-    def quit(self, metadata=None):
-        return Event.QuitEvent(self.agent, time=self.timestamp(), metadata=metadata)
--- mutualfriends/core/event.py
+++
@@ -1,7 +0,0 @@
-from cocoa.core.event import Event as BaseEvent
-
-class Event(BaseEvent):
-    @staticmethod
-    def SelectionEvent(agent, data, time=None):
-        return Event(agent, time, 'select', data)
-
